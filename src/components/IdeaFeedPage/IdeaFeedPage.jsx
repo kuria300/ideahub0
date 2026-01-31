2c58a7eca80269e9c067000e4d7ffd50661e082f
Anzigale George <anziegeorge4@gmail.com>
2026-02-01 00:57:43 +0300

    fixed IdeaCard import

--- src/components/IdeaFeedPage/IdeaFeedPage.jsx
+++ src/components/IdeaFeedPage/IdeaFeedPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAuthHook } from '../../context/Contextdata';
-import IdeaCard from '../IdeaCard/IdeaCard';
+import IdeaCard from '../IdeaCard';
 import SearchBar from '../SearchBar/SearchBar';
 import './IdeaFeedPage.css';
 import { useNavigate, useParams } from 'react-router-dom';
