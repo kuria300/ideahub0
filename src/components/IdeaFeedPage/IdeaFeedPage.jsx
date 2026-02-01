0c1a8df5e71f0e46ac1dfe5dd0bb5cf2fc848e1e
Anzigale George <anziegeorge4@gmail.com>
2026-02-01 17:49:25 +0300

    passed searchTerm to NavBar

--- src/components/IdeaFeedPage/IdeaFeedPage.jsx
+++ src/components/IdeaFeedPage/IdeaFeedPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAuthHook } from '../../context/Contextdata';
 import IdeaCard from '../IdeaCard';
-import SearchBar from '../SearchBar/SearchBar';
 import './IdeaFeedPage.css';
 import { useNavigate, useParams } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
@@ -47,7 +46,7 @@ const IdeaFeedPage = () => {
 
     return (
         <>
-            <Navbar id={id} />
+        <Navbar id={id} searchTerm={searchTerm}/>
             <div className="feed-container">
                 <div className="ideas-list">
                     {filteredIdeas.length > 0 ? (
